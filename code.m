b74ce742cbe6949474e901ab647dac8dbdedb4c8
Michael Castanieto <michaelcastanieto@h316.aau1x.klient.sydhavn.site.aau.dk>
2017-01-03 19:54:13 +0100

    added correlation graphs, PCA, updated minitodo

--- code.m
+++ code.m
@@ -25,6 +25,7 @@ nonSkinG = G(skinClass==2);
 nonSkinB = B(skinClass==2);
 
 data = [skinR skinG skinB; nonSkinR nonSkinG nonSkinB];
+
 labs = genlab([sampleSkinLength, sampleNonSkinLength]);
 z = prdataset(data,labs);
 z = setlablist(z, char('Non Skin','Skin'));
@@ -32,8 +33,57 @@ z = setfeatlab(z, char('R','G','B'));
 z = setprior(z,[sampleSkinLength; sampleNonSkinLength]/sampleLength);
 z = setname(z, 'Skin Segmentation');
 
+% plot feature vectors against each other and
+% calculate the correlation to see relations among the features
+dataRG = [skinR skinG; nonSkinR nonSkinG];
+corrRG = corr(dataRG)
+zRG = prdataset(dataRG,labs);
+zRG = setlablist(zRG, char('Non Skin','Skin'));
+zRG = setfeatlab(zRG, char('R','G'));
+zRG = setprior(zRG,[sampleSkinLength; sampleNonSkinLength]/sampleLength);
+zRG = setname(zRG, 'Skin Segmentation');
+figure
+scatterd(zRG,'legend')
+
+dataRB = [skinR skinB; nonSkinR nonSkinB];
+corrRB = corr(dataRB)
+zRB = prdataset(dataRB,labs);
+zRB = setlablist(zRB, char('Non Skin','Skin'));
+zRB = setfeatlab(zRB, char('R','B'));
+zRB = setprior(zRB,[sampleSkinLength; sampleNonSkinLength]/sampleLength);
+zRB = setname(zRB, 'Skin Segmentation');
+figure
+scatterd(zRB,'legend')
+
+dataGB = [skinG skinB; nonSkinG nonSkinB];
+corrGB = corr(dataGB)
+zGB = prdataset(dataGB,labs);
+zGB = setlablist(zGB, char('Non Skin','Skin'));
+zGB = setfeatlab(zGB, char('G','B'));
+zGB = setprior(zGB,[sampleSkinLength; sampleNonSkinLength]/sampleLength);
+zGB = setname(zGB, 'Skin Segmentation');
+figure
+scatterd(zGB,'legend')
+
 [x, y] = gendat(z,[round(sampleNonSkinLength*0.80) round(sampleSkinLength*0.80)]);
 
 w1 = qdc(y);
 w2 = ldc(y);
 w3 = nmsc(y);
+
+% principle component analysis
+S = cov(data); % calculate covariance matrix
+[V D] = eig(S); % perform eigenvalue decomposition
+eigval = diag(D); [eigval idx] = sort(eigval,'descend');
+V = V(:,idx);
+f = figure; plot(eigval);
+set(gca, 'XTick', [1 2 3], 'XTickLabel', {'1.','2.','3.'})
+
+f = figure;
+linmod = {'magenta','red:','black-.'}
+hold on;
+for j = 1:3
+	plot(V(:,j),linmod{j})
+end
+set(gca,'XTick',[1 2 3],'XTickLabel',{'R','G','B'})
+legend('1. Eigvec','2. Eigvec','3. Eigvec')
